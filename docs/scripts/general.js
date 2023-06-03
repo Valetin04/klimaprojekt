7a4cfc04dc20b9079deecbedcafb6f8737fc4695
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-06-03 11:16:55 +0200

    lock scrolling

--- docs/scripts/general.js
+++ docs/scripts/general.js
@@ -1,5 +1,10 @@
 document.querySelectorAll("img").forEach(e => {
     e.addEventListener("click", event => {
         event.target.parentNode.classList.toggle("image-opened")
+        
+        if(event.target.parentNode.classList.contains("image-opened"))
+            document.body.setAttribute("style", "overflow:hidden;")
+        else
+            document.body.removeAttribute("style")
     })
 })
